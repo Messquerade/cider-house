042c106be65e6cfcb31885ab27a023d099d7200d
Messquerade <annac.klingberg@gmail.com>
2021-11-19 09:09:48 -0800

    add initial component structure

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import Header from './Header';
+import CiderControl from './CiderControl';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      < Header />
+      < CiderControl />
+    </div>
+  );
+}
+
+export default App;
